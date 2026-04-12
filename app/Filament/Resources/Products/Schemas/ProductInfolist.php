22fb1c933d1dea3375878a10bab63dd369f10090
Bima Adiwijaya <bimaadiwijaya83@gmail.com>
2026-04-12 20:49:25 +0700

    Filament_Jobsheet9

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -7,6 +7,8 @@ use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 
 class ProductInfolist
 {
@@ -14,7 +16,65 @@ class ProductInfolist
     {
         return $schema
             ->components([
+                Tabs::make('Product Tabs')
+                    ->tabs([
+                        Tab::make('Product Details')
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->label('Product Name')
+                                    ->weight('bold')
+                                    ->color('primary'),
 
+                                TextEntry::make('id')
+                                    ->label('Product ID'),
+
+                                TextEntry::make('sku')
+                                    ->label('Product SKU')
+                                    ->badge()
+                                    ->color('success'),
+
+                                TextEntry::make('description')
+                                    ->label('Product Description'),
+
+                                TextEntry::make('created_at')
+                                    ->label('Product Creation Date')
+                                    ->date('d M Y')
+                                    ->color('info'),
+                            ]),
+                        Tab::make('Product Price and Stock')
+                            ->schema([
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-s-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock'),
+                            ]),
+                        Tab::make('Image and Status')
+                            ->schema([
+                                ImageEntry::make('image')
+                                    ->label('Product Image')
+                                    ->disk('public'),
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-s-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                IconEntry::make('is_active')
+                                    ->label('Is Active?')
+                                    ->boolean(),
+                                IconEntry::make('is_featured')
+                                    ->label('Is Featured?')
+                                    ->boolean(),
+                            ])
+                    ])
+                    ->columnSpanFull()
+                    ->vertical(),
                 Section::make('Product Info')
                     ->schema([
                         TextEntry::make('name')
@@ -28,7 +88,7 @@ class ProductInfolist
                         TextEntry::make('sku')
                             ->label('Product SKU')
                             ->badge()
-                            ->color(fn ($state) => match (true) {
+                            ->color(fn($state) => match (true) {
                                 str_contains($state, 'A') => 'success',
                                 str_contains($state, 'B') => 'warning',
                                 default => 'gray',
@@ -52,12 +112,12 @@ class ProductInfolist
                             ->weight('bold')
                             ->color('primary')
                             ->icon('heroicon-o-banknotes')
-                            ->formatStateUsing(fn ($state) => 'Rp ' . number_format($state, 0, ',', '.')),
+                            ->formatStateUsing(fn($state) => 'Rp ' . number_format($state, 0, ',', '.')),
 
                         TextEntry::make('stock')
                             ->label('Product Stock')
                             ->icon('heroicon-o-cube')
-                            ->color(fn ($state) => $state > 0 ? 'success' : 'danger'),
+                            ->color(fn($state) => $state > 0 ? 'success' : 'danger'),
                     ])
                     ->columns(2) // ✅ biar sejajar
                     ->columnSpanFull(),
